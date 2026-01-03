8c26cf3c45d6b0b0d8d06af891286ad4e4a2a184
Keven Fonseca <mkfonseca97@gmail.com>
2026-01-03 13:59:44 -0100

    fix(app): Fix the express.static

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const userRoute = require('./routes/userRoute')
 const startBirthdayCron = require('./jobs/birthdayCron')
 const { verifyEmailTransporter } = require('./services/transporter')
@@ -17,9 +18,13 @@ startAppJobs()
 // middlewares
 app.use(cors())
 app.use(express.json())
-app.use(express.static('./public'))
+app.use(express.static(path.join(__dirname, '..', 'public')))
 
 // routes
 app.use('/user', userRoute)
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
+})
 
 module.exports = app
